0806168e662ea3a701602d2b07313b3b2e7d29a4
Sarah Whalley <s.whalley1130@gmail.com>
2023-02-27 14:44:27 -0700

    Actully allows user to delete question from server

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -9,15 +9,35 @@ function QuestionItem({ question, questionDeleted }) {
     </option>
   ));
 
+  function handleChange(e)
+  {
+    console.log(e.target.value)
+  }
+
+  function handleDelete()
+  {
+    fetch(`http://localhost:4000/questions/${question.id}`,
+    {
+      method:'DELETE',
+      headers:
+      {
+        "Content-Type": "application/json",
+        "Accepts": "application/json"
+      },
+      body:JSON.stringify(question)
+    })
+    .then(questionDeleted(question))
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={handleChange} defaultValue={correctIndex}>{options}</select>
       </label>
-      <button onClick={()=>questionDeleted(question)}>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
